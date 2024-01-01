3e41894917ec093cb519c28b94a08c9c8d23bdbc
Gladys Romero <111000044+Gladys077@users.noreply.github.com>
2024-01-01 20:20:56 -0300

    mejoré la section Herramientas

---
+++ script.js
@@ -0,0 +1,10 @@
+// Obtener todos los enlaces dentro del menú
+const enlacesMenu = document.querySelectorAll('nav ul li a');
+
+// Recorrer cada enlace y agregar un evento de clic
+enlacesMenu.forEach(enlace => {
+    enlace.addEventListener('click', () => {
+        // Cerrar el menú al hacer clic en cualquier enlace
+        document.getElementById('check').checked = false;
+    });
+});
